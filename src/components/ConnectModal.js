8a7ec019dbaba25b3984f0f5de4199dca5fd63e5
Arnaud293 <arnaudlt293@gmail.com>
2022-11-27 14:39:24 +0100

    preparing ConnectModal and sub components, add dynamic style on btn

---
+++ src/components/ConnectModal.js
@@ -0,0 +1,17 @@
+import React, { useState } from 'react';
+
+const ConnectModal = () => {
+
+    const [signUp, setSignUp] = useState(true);
+
+    return (
+        <div className="connect-modal">
+            <div className="header-btn">
+                <button style={{background: signUp ? "rgb(28,28,28)" : "rgb(83,83,83)"}}>S'inscrire</button>
+                <button style={{background: signUp ?  "rgb(83,83,83)" : "rgb(28,28,28)"}}>Se connecter</button>
+            </div>
+        </div>
+    );
+};
+
+export default ConnectModal;
