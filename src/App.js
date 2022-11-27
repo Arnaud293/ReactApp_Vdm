ff45cddd59089d2fa047507c860c2b6e9b28c0e4
Arnaud293 <arnaudlt293@gmail.com>
2022-11-27 14:38:51 +0100

    link connectModal

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import ConnectModal from './components/ConnectModal';
 
 const App = () => {
   return (
     <div>
-      
+      <div className="app-header">
+        <ConnectModal />
+      </div>
+      <div className="posts-container"></div>
     </div>
   );
 };
