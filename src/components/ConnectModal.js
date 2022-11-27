42e939b4cfb03948c6afc9b054f4ff800d61c584
Arnaud293 <arnaudlt293@gmail.com>
2022-11-27 14:44:05 +0100

    add function on btn, display the appropriate sub-component

--- src/components/ConnectModal.js
+++ src/components/ConnectModal.js
@@ -1,4 +1,6 @@
 import React, { useState } from 'react';
+import Login from './Login';
+import SignUp from './SignUp';
 
 const ConnectModal = () => {
 
@@ -7,9 +9,14 @@ const ConnectModal = () => {
     return (
         <div className="connect-modal">
             <div className="header-btn">
-                <button style={{background: signUp ? "rgb(28,28,28)" : "rgb(83,83,83)"}}>S'inscrire</button>
-                <button style={{background: signUp ?  "rgb(83,83,83)" : "rgb(28,28,28)"}}>Se connecter</button>
+                <button 
+                    style={{background: signUp ? "rgb(28,28,28)" : "rgb(83,83,83)"}} 
+                    onClick={() => setSignUp(true)}>S'inscrire</button>
+                <button 
+                    style={{background: signUp ?  "rgb(83,83,83)" : "rgb(28,28,28)"}}
+                    onClick={() => setSignUp(false)}>Se connecter</button>
             </div>
+            {signUp ? <SignUp/> : <Login />}
         </div>
     );
 };
