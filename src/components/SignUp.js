0431b5967b89507ca006539364142f90ee43c195
Arnaud293 <arnaudlt293@gmail.com>
2022-11-27 14:51:34 +0100

    SignUp JSX structure

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -2,10 +2,18 @@ import React from 'react';
 
 const SignUp = () => {
     return (
-        <div>
-            <h1>Sign Up</h1>
+        <div className="signup-container">
+            <div className="signup">
+                <h3>S'inscrire</h3>
+                <form>
+                    <input type="text" placeholder='Pseudo' required />
+                    <input type="email" placeholder='Email' required />
+                    <input type="password" placeholder='Mot de passe' required />
+                    <input type="submit" value="Valider l'inscription" />
+                </form>
+            </div>
         </div>
     );
 };
 
-export default SignUp;<h1>Sign Up</h1>
+export default SignUp;
