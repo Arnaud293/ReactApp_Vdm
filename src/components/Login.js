2cb7b419969d39ff6c4fd588419088b480e9b120
Arnaud293 <arnaudlt293@gmail.com>
2022-11-28 16:54:10 +0100

    add ref for email and password

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,13 +1,17 @@
-import React from 'react';
+import React, { useRef } from 'react';
 
 const Login = () => {
+
+    const loginEmail = useRef();
+    const loginPassword = useRef();
+
     return (
         <div className="login-container">
             <div className="login">
                 <h3>Se connecter</h3>
                 <form>
-                    <input type="email" placeholder='Email' required/>
-                    <input type="password" placeholder='Mot de passe' required />
+                    <input type="email" placeholder='Email' required ref={loginEmail}/>
+                    <input type="password" placeholder='Mot de passe' required ref={loginPassword}/>
                     <input type="submit" value="Connexion" />
                 </form>
             </div>
