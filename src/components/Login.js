b3a4ffd5a408b3900bbfe1b6e0ac396ff8b36ed0
Arnaud293 <arnaudlt293@gmail.com>
2022-11-28 16:52:55 +0100

    JSX structure

--- src/components/Login.js
+++ src/components/Login.js
@@ -2,8 +2,15 @@ import React from 'react';
 
 const Login = () => {
     return (
-        <div>
-            <h1>Login</h1>
+        <div className="login-container">
+            <div className="login">
+                <h3>Se connecter</h3>
+                <form>
+                    <input type="email" placeholder='Email' required/>
+                    <input type="password" placeholder='Mot de passe' required />
+                    <input type="submit" value="Connexion" />
+                </form>
+            </div>
         </div>
     );
 };
